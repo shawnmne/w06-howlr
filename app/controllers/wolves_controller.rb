a77d25faefd2a0091b3a0c48fd19e809830b91e7
Shawn Michaud <shawnmne@hotmail.com>
2016-04-22 00:53:26 -0500

    new wolf view built

---
+++ app/controllers/wolves_controller.rb
@@ -0,0 +1,20 @@
+get '/wolves' do
+	@wolves = Wolf.all 			
+	erb :"wolves/index"
+end
+
+#new
+get '/wolves/new' do 
+  @wolf = Wolf.new
+  erb :"wolves/new"
+end
+
+#create
+post '/wolves' do
+  @wolf = Wolf.new(params)
+  if @wolf.save
+    redirect to('/wolves')
+  else
+    erb :"wolves/new"
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 # For example:
 #
 # Insect.create!(name: "Fuzzy Mantis", description: "Really Fierce")
+
+Wolf.create(name: "Howler", image: "http://dreamatico.com/data_images/wolf/wolf-2.jpg", description: "very hairy")
+Wolf.create(name: "Randy", image: "http://dreamatico.com/data_images/wolf/wolf-3.jpg", description: "very scary")
+Wolf.create(name: "Bob", image: "http://dreamatico.com/data_images/wolf/wolf-5.jpg", description: "very nosy")
+Wolf.create(name: "Sally", image: "http://dreamatico.com/data_images/wolf/wolf-6.jpg", description: "very big")
+Wolf.create(name: "Luther", image: "http://dreamatico.com/data_images/wolf/wolf-7.jpg", description: "very brown")
+Wolf.create(name: "Susan", image: "http://dreamatico.com/data_images/wolf/wolf-8.jpg", description: "very funny")
+Wolf.create(name: "Nancy", image: "https://warawdotorg.files.wordpress.com/2014/11/cropped-stylized_wolf-2.jpg", description: "very polite")
